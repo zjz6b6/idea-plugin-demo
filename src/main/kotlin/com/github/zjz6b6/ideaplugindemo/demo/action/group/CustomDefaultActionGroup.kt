6ed08512f3617007384c01a6cd2142e4afaeafe6
zjz6b6 <407091038@qq.com>
2023-07-17 19:42:48 +0800

    add code

--- src/main/kotlin/com/github/zjz6b6/ideaplugindemo/demo/action/group/CustomDefaultActionGroup.kt
+++ src/main/kotlin/com/github/zjz6b6/ideaplugindemo/demo/action/group/CustomDefaultActionGroup.kt
@@ -1,6 +1,6 @@
-package com.github.zjz6b6.ideaplugindemo.demo.action
+package com.github.zjz6b6.ideaplugindemo.demo.action.group
 
-import com.github.zjz6b6.ideaplugindemo.demo.MyTools
+import com.github.zjz6b6.ideaplugindemo.demo.SdkIcons
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DefaultActionGroup
@@ -14,7 +14,7 @@ class CustomDefaultActionGroup : DefaultActionGroup() {
     override fun update(e: AnActionEvent) {
         var editor = e.getData(CommonDataKeys.EDITOR)
         e.presentation.isEnabled = (editor != null)
-        e.presentation.icon = MyTools.SdkIcons.Sdk_default_icon
+        e.presentation.icon = SdkIcons.Sdk_default_icon
     }
 
 }
